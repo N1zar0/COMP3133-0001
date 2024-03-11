77e25afa1baf1d4a29b12eac948b3536588d66e9
N1zar0 <atassinizar@gmail.com>
2024-03-10 21:23:47 -0400

    lab5

---
+++ Lab05_101297716/exercise-4/customer.ts
@@ -0,0 +1,24 @@
+export class Customer {
+    private firstName: string;
+    private lastName: string;
+    private _age: number;
+
+    constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this._age = age;
+    }
+
+    greeter() {
+        console.log(`Hello, ${this.firstName} ${this.lastName}`);
+    }
+
+    GetAge() {
+        console.log(`Age: ${this._age}`);
+    }
+}
+
+
+let customer = new Customer("John", "Smith", 30);
+customer.greeter();
+customer.GetAge();
